48c644a6ed828109b398420dcfb01c7246751f53
amarbashashaik521-max <amarbashashaik521@gmail.com>
2026-08-27 18:49:18 +0530

    Create gallory.js

---
+++ gallory.js
@@ -0,0 +1,31 @@
+
+let images = ["welcome.jpeg" , "rohit2.jpeg" , "rohit sharma.jpeg" , "rohit4.jpeg" ,"end.jpeg"];
+</script>
+
+<script>
+let current=0;
+document.getElementById("image").src = images[current];
+function next()
+{
+	current=current+1; 
+	        
+	if(current >= images.length)
+	{
+    		current = 0;
+	}	
+	document.getElementById("image").src = images[current];
+        document.getElementById("page-num").innerHTML="Image - " + (current + 1) + "/ 5" ;
+} 
+function previous()
+{
+	current=current - 1;
+	if(current < 0)
+  	{
+    		current = images.length - 1;
+	}
+
+	document.getElementById("image").src = images[current];
+        document.getElementById("page-num").innerHTML="Image - " + (current + 1) + "/ 5" ;
+}
+
+
